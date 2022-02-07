ebe403b9050305d654f3f308578bb4223ff60557
starzhitskiy <study.html.css.java@gmail.com>
2022-02-07 12:57:14 +0400

    drag_and_drop

--- trello/js/mine.js
+++ trello/js/mine.js
@@ -1,4 +1,5 @@
 const lists = document.querySelectorAll('.list')
+const button = document.querySelector('.button')
 
 function addTask () {
   const btn = document.querySelector('.add__btn')
@@ -45,7 +46,98 @@ function addTask () {
     lists[0].append(newItem)
 
     clear()
+    dragNdrop()
   })
 }
 
 addTask()
+
+// add boards
+
+function addBoard() {
+  const boards = document.querySelector('.boards')
+  const board = document.createElement('div')
+  board.classList.add('boards__item')
+  board.innerHTML = `
+    <span contenteditable="true" class="title">Введите название</span>
+    <div class="list"></div>
+  `
+
+  boards.append(board)
+
+  changeTitle()
+  dragNdrop()
+
+  board.addEventListener('dblclick', () => {
+    board.remove()
+  })
+}
+
+button.addEventListener('click', addBoard)
+
+// click title and clear
+
+function changeTitle() {
+  const titles = document.querySelectorAll('.title')
+
+  titles.forEach( title => {
+    title.addEventListener('click', e => e.target.textContent = '')
+  })
+}
+changeTitle()
+
+// drag end drop
+
+let draggedItem = null
+
+function dragNdrop() {
+  const listItems = document.querySelectorAll('.list__item')
+  const lists = document.querySelectorAll('.list')
+
+  for (let i = 0; i < listItems.length; i++) {
+    const item = listItems[i]
+
+    item.addEventListener('dragstart', () => {
+      draggedItem = item
+      setTimeout(() => {
+        item.style.display = 'none'
+      }, 0)
+    })
+
+    item.addEventListener('dragend', () => {
+      setTimeout(() => {
+        item.style.display = 'block'
+        draggedItem = null
+      }, 0)
+    })
+
+    // delete
+    item.addEventListener('dblclick', () => {
+      item.remove()
+    })
+
+    for (let j = 0; j < lists.length; j++) {
+      const list = lists[j]
+
+      list.addEventListener('dragover', e => {
+        e.preventDefault()
+      })
+
+      list.addEventListener('dragenter', function(e) {
+        e.preventDefault()
+        this.style.backgroundColor = 'rgba(0,0,0, .3)'
+      })
+
+      list.addEventListener('dragleave', function(e) {
+        this.style.backgroundColor = 'rgba(0,0,0, 0)'
+      })
+
+      list.addEventListener('drop', function(e) {
+        this.style.backgroundColor = 'rgba(0,0,0, 0)'
+        this.append(draggedItem)
+      })
+    }
+  }
+}
+
+dragNdrop()
