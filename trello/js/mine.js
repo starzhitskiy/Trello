4c070aa8d027e74d0f0929b1b2d05f1f8630d329
starzhitskiy <study.html.css.java@gmail.com>
2022-02-07 11:19:46 +0400

    add scss

---
+++ trello/js/mine.js
@@ -0,0 +1,49 @@
+const lists = document.querySelectorAll('.list')
+
+function addTask () {
+  const btn = document.querySelector('.add__btn')
+  const addBtn = document.querySelector('.add__item-btn')
+  const cancelBtn = document.querySelector('.cancel__item-btn')
+  const textarea = document.querySelector('.textarea')
+  const form = document.querySelector('.form')
+
+  let value
+
+  btn.addEventListener('click', () => {
+    form.style.display = 'block'
+    btn.style.display = 'none'
+    addBtn.style.display = 'none'
+
+    textarea.addEventListener('input', e => {
+      value = e.target.value
+
+      if (value) {
+        addBtn.style.display = 'block'
+      } else {
+        addBtn.style.display = 'none'
+      }
+    })
+  })
+
+  cancelBtn.addEventListener('click', () => {
+    textarea.value = ''
+    value = ''
+    form.style.display = 'none'
+    btn.style.display = 'flex'
+  })
+
+  addBtn.addEventListener('click', () => {
+    const newItem = document.createElement('div')
+    newItem.classList.add('list__item')
+    newItem.draggable = true
+    newItem.textContent = value
+    lists[0].append(newItem)
+
+    textarea.value = ''
+    value = ''
+    form.style.display = 'none'
+    btn.style.display = 'flex'
+  })
+}
+
+addTask()
