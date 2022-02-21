87261e4d716d2cb4d708c0fb7ea1655ab48e047c
starzhitskiy <study.html.css.java@gmail.com>
2022-02-21 13:42:24 +0400

    test

--- trello/js/mine.js
+++ trello/js/mine.js
@@ -142,30 +142,3 @@ function dragNdrop() {
 
 dragNdrop()
 
-
-const login = document.querySelector('.login')
-const register = document.querySelector('.register')
-const registration = document.querySelector('.forma')
-const forms = document.querySelector('.forms')
-const log = document.querySelector('.button__log')
-const close = document.querySelector('.button__close')
-
-log.addEventListener('click', ()=> {
-  registration.style.display = 'block';
-})
-
-login.addEventListener('click', () => {
-  forms.style.left = '100px';
-  login.style.backgroundColor = '#3c71f2';
-  register.style.backgroundColor = '#222b41';
-})
-
-register.addEventListener('click', () => {
-  forms.style.left = '-210px';
-  register.style.backgroundColor = '#3c71f2';
-  login.style.backgroundColor = '#222b41';
-})
-
-close.addEventListener('click', ()=> {
-  registration.style.display = 'none';
-})
